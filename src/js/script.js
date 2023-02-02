4a7aee4971a956ad6103289b08b1747e2ba75a16
Mateusz Oko <mo89mo@gmail.com>
2023-02-02 19:03:51 +0100

    Exercise 5 - Add filter function and option to hide books

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,7 @@
     },
     containerOf: {
       booksList: '.books-list',
+      filters: '.filters',
     },
     oneBook: {
       title: '.book__name',
@@ -34,17 +35,37 @@
   }
 
   const favoriteBooks = [];
-  // console.log(favoriteBooks);
+  const filters = [];
 
   function initActions(){
     const booksCovers = document.querySelectorAll('.book__image');                // Znajduje wszystkie okładki książek
     // console.log('booksList', booksCovers);
+    const filtersOption = document.querySelector('.filters');                     // Znajduje wszystkie filtry książek
+    // console.log('filtersSection', filtersSection);
+    filtersOption.addEventListener('click', function(event){                      // Event - kliknięcie będzie wywoływało akcję (zaznaczenie filtra)
+      const booksFilter = event.target;                                           // Event delegation II - namierza checkbox z kontenera i zaznacza go po kliknięiu
+      // console.log('booksFilter', booksFilter);
+      if(booksFilter.tagName == 'INPUT' &&                                        // Obwarowania, które musi spełniać checkbox
+          booksFilter.name == 'filter' && 
+          booksFilter.type == 'checkbox'){  
+        const filterValue = booksFilter.value;                                    // Wyświetla wartość checkboxa                                    
+        console.log(filterValue);
+        if(booksFilter.checked == true){                                          // Dodanie do tablicy (filters) po kliknięciu warości jeśli spełnia wymogi i nie jest zaznaczona
+          filters.push(filterValue);
+        } else {
+          filters.splice(filters.indexOf(filterValue), 1);                        // Usunięcie z tablicy zawartości po ponownym kliknięciu
+        }
+        console.log('filters', filters);
+      }
+      filterBooks();                                                              // Wywołanie funkcji filtrującej książki
+    });
+
     for(let cover of booksCovers){                                                // Pętla do wyszukania okładek
       cover.addEventListener('dblclick', function(event){                         // Event - podwójne kliknięcie będzie wywoływało akcję
         event.preventDefault();                                                   // Zatrzymuje domyślne zachowanie przeglądarki
         const coverLink = event.target;                                           // Event delegation - namierza konkretny element w kontenerze, który wykona akcję po podwójnym kliknięciu
         // console.log('coverLink:', coverLink);
-        if (coverLink.offsetParent.classList.contains('book__image')){            // Dodanie zależności dla konkretnego elementu w celu:
+        if(coverLink.offsetParent.classList.contains('book__image')){             // Dodanie zależności dla konkretnego elementu w celu:
           // console.log('event.target:', event.target);
           const bookId = coverLink.offsetParent.getAttribute('data-id');          // Znalezienie Id książki po podwójnym kliknięciu
           // console.log('bookId:', bookId);
@@ -55,8 +76,30 @@
             favoriteBooks.splice(favoriteBooks.indexOf(bookId), 1);               // Jeżeli książka ma klasę favorite nastąpi przywrócenie tablicy do stanu przed dodaniem klasy a następnie... 
             coverLink.offsetParent.classList.remove('favorite');                  // ... Usunięcie klasy 'favorite' po podwójnym kliknięciu w okładkę
           }
+          console.log(favoriteBooks);
         }
       });
+    }
+  }
+  
+  function filterBooks(){                                                         // Funkcja do filtrowania książek
+    for (const book of dataSource.books){                                         // Pętla, która przejdzie po wszystkich książkach z dataSource.books 
+      let shouldBeHidden = false;                                                 // Zmienna, która zostanie wykorzystana do dodania opcji hidden - stąd jest false
+      const selectCover = document.querySelector(                                 // Element .book__image potrzebny do 
+        '.book__image[data-id="' + book.id + '"]'
+      );
+      for(const filter of filters){                                               // Pętla, która przejdzie po tablicy filters i ustali, czy dany filtr pasuje do informacji o danej książce 
+        if(!book.details[filter]){                                                // Jeśli dana właściwość powinna być true, a nie jest, ...
+          shouldBeHidden = true;                                                  // ... to należy zmienić shouldBeHidden na true i przerwać działanie pętli
+          break;                                                                  // Przerwanie pętli
+        }
+      }
+      if(shouldBeHidden){                                                         // Pętla warunkowa, która sprawdza wartość shouldBeHidden
+        selectCover.classList.add('hidden');                                      // Jeśli jest równa true, to należy znaleźć element .book__image danej książki i nadać mu klasę hidden
+      } else {
+        selectCover.classList.remove('hidden');                                   // Jeśli jest równa false, to należy taką klasę zabrać.
+      }
+      console.log('selectCover:', selectCover);
     }
   }
 
